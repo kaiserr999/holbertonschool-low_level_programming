762d386ffc0229a088d44170bd1e8589a7a13f87
Kaiserr999 <I.camelin74@gmail.com>
2026-02-24 14:40:33 +0100

    add

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main : Entry point
+ * description : ecrire tous les char en maj et min
+ * return: Always 0 "success"
+ */
+ int main(void)
+ {
+	 char lettre;
+
+	 for (lettre = 'a'; lettre <= 'z'; lettre++)
+         {
+		 putchar(lettre);
+	 }
+	 for (lettre = 'A'; lettre <= 'Z'; lettre ++)
+	 {
+		 putchar(lettre);
+	 }
+	 
+	 putchar('n\');
+
+	 return (0);
+ }
