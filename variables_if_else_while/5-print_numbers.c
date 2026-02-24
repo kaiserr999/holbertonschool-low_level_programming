13451249c9c0f8f594460b36da3fd003e17b3b0f
Kaiserr999 <I.camelin74@gmail.com>
2026-02-24 15:31:24 +0100

    add

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main: starting point
+ * description: addiche les chiffre de 0 à 9
+ * return: always 0
+ */
+int main(void)
+{
+	int n;
+	for (n = 0; n <= 9; n++);
+	{
+		printf("%d", n);
+	}
+	printf("/n");
+
+	return (0);
+}
