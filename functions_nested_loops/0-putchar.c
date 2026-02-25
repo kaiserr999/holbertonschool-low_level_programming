0c5effbfe7ba34003c1296703a24fa8504ce95c8
Kaiserr999 <I.camelin74@gmail.com>
2026-02-25 13:47:58 +0100

    add

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
 /**
  * main - Entry point
- * description: le programme doit afficher _putchar suivi d'un retour a la ligne
- * return: always 0 (success)
+ * Description: le programme doit afficher _putchar suivi
+ *              d'un retour a la ligne
+ * Return: always 0 (success)
  */
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
