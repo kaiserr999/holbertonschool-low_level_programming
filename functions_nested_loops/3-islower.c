6b148d7b140c8fe1e4c265db34a810639eeebf60
Kaiserr999 <I.camelin74@gmail.com>
2026-02-25 16:18:59 +0100

    add

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,21 @@
-z
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: le programme doit checker si le caractere est en minuscule ou pas
+ *              suivi d'un retour a la ligne
+ * Return: always 0 (success)
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
