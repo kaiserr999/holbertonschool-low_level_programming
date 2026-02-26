87a6eb74341b486995e5c1ab0b5ee59a52c48713
Kaiserr999 <I.camelin74@gmail.com>
2026-02-26 13:55:51 +0100

    add 4 script

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet 10 times in lowercase
+ * print_alphabet_x10 - prints the alphabet in lowercase 10 times
+ *                 followed by a newline
  * Return: void
  */
 void print_alphabet_x10(void)
 {
-int i;
-char c;
+    int i;
+    char c;
 
-for (i = 0; i < 10; i++)
-{
-for (c = 'a'; c <= 'z'; c++)
-_putchar(c);
-_putchar('\n');
-}
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+            _putchar(c);
+        _putchar('\n');
+    }
 }
---
+++ functions_nested_loops/sign_main.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+    printf("return: %d\n", print_sign(-10));
+    printf("return: %d\n", print_sign(0));
+    printf("return: %d\n", print_sign(10));
+    return 0;
+}
