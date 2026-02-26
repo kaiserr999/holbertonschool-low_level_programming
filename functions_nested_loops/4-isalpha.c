d63377b0753c1905a2b7f3e68d51430ec3a88169
Kaiserr999 <I.camelin74@gmail.com>
2026-02-26 10:42:18 +0100

    add 4 script

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: le programme doit afficher 1 si c est une lettre, majuscule ou minuscule
+ * Return: always 0 (success)
+ */
+int _isalpha(int c=)
+{
+
+
+
+    
+}
+
