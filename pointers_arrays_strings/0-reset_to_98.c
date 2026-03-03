0ad29a20701e0ba4a47e10256db2c5bd0c76b647
Kaiserr999 <I.camelin74@gmail.com>
2026-03-03 14:26:47 +0100

    add

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - prend un pointeur vers un int et met à jour
+ * la valeur vers laquelle il pointe à 98.
+ * @n: le pointeur vers l'entier à modifier
+ *
+ * Return: void (rien)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Description: Header file containing prototypes for all functions
+ * used in the pointers_arrays_strings directory.
+ */
+
+void reset_to_98(int *n);
+
+#endif
