753d4179d21962afb7c31f0b6554e62e960f4293
Kaiserr999 <I.camelin74@gmail.com>
2026-03-04 17:22:47 +0100

    add simple calculator skeleton

---
+++ simple-calculator/README.md
@@ -0,0 +1,15 @@
+# Simple Calculator
+
+This program prints a calculator menu and allows quitting with option `0`.
+
+## Build
+
+```sh
+gcc -Wall -Wextra -Werror -pedantic -std=gnu89 calculator.c -o calculator
+```
+
+## Run
+
+```sh
+./calculator
+```
---
+++ simple-calculator/calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - simple calculator skeleton program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int choice;
+
+	printf("Simple Calculator\n");
+	printf("1) Add\n");
+	printf("2) Subtract\n");
+	printf("3) Multiply\n");
+	printf("4) Divide\n");
+	printf("0) Quit\n");
+	printf("Choice: ");
+
+	if (scanf("%d", &choice) != 1)
+	{
+		printf("Bye!\n");
+		return (0);
+	}
+
+	if (choice == 0)
+		printf("Bye!\n");
+
+	return (0);
+}
