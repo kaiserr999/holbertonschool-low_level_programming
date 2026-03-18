20f36c3bab5f0e6dd1bece5a6e5c292d6dfec474
Kaiserr999 <I.camelin74@gmail.com>
2026-03-18 15:07:35 +0100

    add

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -10,36 +10,34 @@
 void print_all(const char * const format, ...)
 {
     va_list args;
-    int i;
+    int i, j;
     char *str, *sep;
-
+    
+    va_start(args, format);
     i = 0;
     sep = "";
-    va_start(args, format);
     while (format && format[i])
     {
-        if (format[i] == 'c')
+        j = 0;
+        while ("cifs"[j])
         {
-            printf("%s%c", sep, va_arg(args, int));
-            sep = ", ";
-        }
-        if (format[i] == 'i')
-        {
-            printf("%s%d", sep, va_arg(args, int));
-            sep = ", ";
-        }
-        if (format[i] == 'f')
-        {
-            printf("%s%f", sep, va_arg(args, double));
-            sep = ", ";
-        }
-        if (format[i] == 's')
-        {
-            str = va_arg(args, char *);
-            if (str == 0)
-                str = "(nil)";
-            printf("%s%s", sep, str);
-            sep = ", ";
+            if (format[i] == "cifs"[j])
+            {
+                printf("%s", sep);
+                if (j == 0)
+                    printf("%c", va_arg(args, int));
+                if (j == 1)
+                    printf("%d", va_arg(args, int));
+                if (j == 2)
+                    printf("%f", va_arg(args, double));
+                if (j == 3)
+                {
+                    str = va_arg(args, char *);
+                    printf("%s", str ? str : "(nil)");
+                }
+                sep = ", ";
+            }
+            j++;
         }
         i++;
     }
