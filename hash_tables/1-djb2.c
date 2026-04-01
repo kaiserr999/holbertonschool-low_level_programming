843fb9564c1c8ef4e57a7436dcc293dc07c59222
Kaiserr999 <I.camelin74@gmail.com>
2026-04-01 13:39:49 +0200

    Add function documentation for hash_djb2 to fix Betty doc warning

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+ * hash_djb2 - hash function implementing the djb2 algorithm
+ * @str: string to hash
+ *
+ * Return: hash value
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
